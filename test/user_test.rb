d75d7f7b6e3322c16a9583656008f8172d769802
davisc0801 <davisc1014@gmail.com>
2019-03-20 09:29:15 -0600

    Complete tests for user class

--- test/user_test.rb
+++ test/user_test.rb
@@ -15,33 +15,21 @@ class UserTest < Minitest::Test
     assert_instance_of User, @sal
   end
 
-end
+  def test_user_has_a_name_from_argument
+    assert_equal "Sal", @sal.name
+  end
 
-#
-# pry(main)> require './lib/joke'
-# # => true
-#
-# pry(main)> require './lib/user'
-# # => true
-#
-# pry(main)> sal = User.new("Sal")
-# # => #<User:0x00007fb71e1eb8d8...>
-#
-# pry(main)> sal.name
-# # => "Sal"
-#
-# pry(main)> sal.jokes
-# # => []
-#
-# pry(main)> joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
-# # => #<Joke:0x00007fb71da169f0...>
-#
-# pry(main)> joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
-# # => #<Joke:0x00007fb71d8e0bd0...>
-#
-# pry(main)> sal.learn(joke_1)
-#
-# pry(main)> sal.learn(joke_2)
-#
-# pry(main)> sal.jokes
-# # => [#<Joke:0x00007fb71da169f0...>, #<Joke:0x00007fb71d8e0bd0...>]
+  def test_user_starts_with_no_jokes_known
+    assert_equal [], @sal.jokes
+  end
+
+  def test_user_can_learn_jokes
+    assert_equal [], @sal.jokes
+
+    @sal.learn(@joke1)
+    assert_equal [@joke1], @sal.jokes
+
+    @sal.learn(@joke2)
+    assert_equal [@joke1, @joke2], @sal.jokes
+  end
+end
