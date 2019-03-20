63095cf6d794466e992fcfcbd0ec3d440ce7997f
davisc0801 <davisc1014@gmail.com>
2019-03-20 10:11:16 -0600

    Complete open mic class tests

--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -17,44 +17,35 @@ class OpenMicTest < Minitest::Test
   def test_it_exists
     assert_instance_of OpenMic, @open_mic
   end
-end
 
-# pry(main)> open_mic.location
-# # => "Comedy Works"
-#
-# pry(main)> open_mic.date
-# # => "11-20-18"
-#
-# pry(main)> open_mic.performers
-# # => []
-#
-# pry(main)> sal = User.new("Sal")
-# # => #<User:0x00007fe8fda12a00...>
-#
-# pry(main)> ali = User.new("Ali")
-# # => #<User:0x00007fe8ff0dddc0...>
-#
-# pry(main)> open_mic.welcome(sal)
-#
-# pry(main)> open_mic.welcome(ali)
-#
-# pry(main)> open_mic.performers
-# # => [#<User:0x00007fe8fda12a00...>, #<User:0x00007fe8ff0dddc0...>]
-#
-# pry(main)> joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
-# # => #<Joke:0x00007fe8fd892978...>
-#
-# pry(main)> joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
-# # => #<Joke:0x00007fe8fe19f250...>
-#
-# pry(main)> ali.learn(joke_1)
-#
-# pry(main)> ali.learn(joke_2)
-#
-# pry(main)> open_mic.repeated_jokes?
-# # => false
-#
-# pry(main)> ali.tell(sal, joke_1)
-#
-# pry(main)> open_mic.repeated_jokes?
-# # => true
+  def test_it_has_a_location
+    assert_equal "Comedy Works", @open_mic.location
+  end
+
+  def test_it_has_a_date
+    assert_equal "11-20-18", @open_mic.date
+  end
+
+  def test_it_starts_with_no_performers
+    assert_equal [], @open_mic.performers
+  end
+
+  def test_it_can_add_performers
+    assert_equal [], @open_mic.performers
+
+    @open_mic.welcome(@sal)
+    assert_equal [@sal], @open_mic.performers
+
+    @open_mic.welcome(@ali)
+    assert_equal [@sal, @ali], @open_mic.performers
+  end
+
+  def test_if_jokes_are_repeated
+    @ali.learn(@joke1)
+    @ali.learn(@joke2)
+    refute @open_mic.repeated_jokes?
+
+    @ali.tell(@sal, @joke1)
+    assert @open_mic.repeated_jokes?
+  end
+end
