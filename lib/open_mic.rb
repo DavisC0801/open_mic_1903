c5c1798efd6d8c414ac80e3434c16bad37eae650
davisc0801 <davisc1014@gmail.com>
2019-03-20 10:13:44 -0600

    Add initialize and reader for open mic class

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -3,4 +3,12 @@ require "./lib/user.rb"
 
 class OpenMic
 
+attr_reader :location, :date, :performers
+
+def initialize(venue)
+  @location = venue[:location]
+  @date = venue[:date]
+  @performers = []
+end
+
 end
