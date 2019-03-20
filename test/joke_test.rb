142d3fc54e532de89b0fe926fc795756617c4704
davisc0801 <davisc1014@gmail.com>
2019-03-20 09:09:30 -0600

    Create skeletons for Joke class and test

---
+++ test/joke_test.rb
@@ -0,0 +1,28 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/joke.rb"
+
+class JokeTest < Mdinitest::Test
+
+
+
+end
+
+
+
+
+# ruby
+# pry(main)> require './lib/joke'
+# # => true
+#
+# pry(main)> joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+# # => #<Joke:0x00007f84602e4190...>
+#
+# pry(main)> joke.id
+# # => 1
+#
+# pry(main)> joke.setup
+# # => "Why did the strawberry cross the road?"
+#
+# pry(main)> joke.punchline
+# # => "Because his mother was in a jam."
